df080b5b7427b7bf8eea6e01c76ac1f8ae07d234
crow1011 <crowsmith1011@gmail.com>
2019-11-04 15:39:27 +0300

    add send tg messages and logging

--- sirenaSrv.py
+++ sirenaSrv.py
@@ -1,17 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+import logging
+import sendTG, stngs
+import hashlib
+
+logging.basicConfig(filename="sirena.log", level=logging.ERROR, format='%(asctime)s;%(levelname)s;%(message)s')
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-@app.route('/tg')
+@app.route('/tg', methods=['POST'])
 def tg():
-    return "tg"
-
+	try:
+		if request.json['token']==hashlib.md5(stngs.sirenaToken()['token'].encode('utf-8')).hexdigest():
+			sendTG.SendTG(request.json['msg'])
+			return "tg"
+		else:
+			logging.error("Invalid sirena token in request data. Check sirena token config in stngs.py and your sirena client config")
+			return "read log"
+	except:
+		logging.error("No sirena token in request data. Check sirena token config in stngs.py and your sirena client config")
+		return "read log"
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host=stngs.hoststngs()['ip'])
