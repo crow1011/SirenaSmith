fdaaa8591d0c8bd15381266e30f17fdc3be1d8f7
crow1011 <50404813+crow1011@users.noreply.github.com>
2019-11-04 15:49:02 +0300

    Create stngs.py

---
+++ stngs.py
@@ -0,0 +1,14 @@
+def sirenaToken():
+	sirenaToken = {'token':''}  #set up sirena token, str
+	return sirenaToken
+
+
+def tgstngs():
+	tg = {'token':'',   #set up telegram api token, str
+	'chatid':}          #set up your chat id, int
+	return tg
+
+
+def hoststngs():
+	host = {'ip':'127.0.0.1'}    #change this ip-address to yours
+	return host
