d126bc05b43bf8628618396e1b1b8ee93cc1d5da
crow1011 <crowsmith1011>
2020-05-24 13:22:40 +0300

    corrected systemd config create

--- sirena/sirena.sh
+++ sirena/sirena.sh
@@ -9,8 +9,7 @@ mkdir /var/log/sirena
 touch /var/log/sirena/sirenasrv.log
 mv /etc/sirena/sirena.yaml /etc/sirena/sirena.yaml.save
 cp /opt/SirenaSmith/sirena/sirena.yaml.example /etc/sirena/sirena.yaml
-cp /opt/SirenaSmith/configs/systemd/sirena.service.example /etc/systemd/system/sirena.service
-sed 's/SIRENA_GROUP/nginx/g' /etc/systemd/system/sirena.service
+sed 's/SIRENA_GROUP/'$GROUP'/g' /opt/SirenaSmith/configs/systemd/sirena.service.example > /etc/systemd/system/sirena.service
 mv /etc/nginx/conf.d/sirena.conf /etc/nginx/conf.d/sirena.conf.save
 cp /opt/SirenaSmith/configs/nginx/sirena.conf.example /etc/nginx/conf.d/sirena.conf
 chown -R $GROUP:$GROUP /opt/SirenaSmith/
